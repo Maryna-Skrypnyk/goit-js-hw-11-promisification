d53d7a96a13996b896a05b0aa2979d24f3d2a65e
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-05-20 13:09:02 +0300

    Task-2 done

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,13 +1,13 @@
-const delay = ms => {
-  // Твой код
-  return new Promise(resolved => {
+export const delay = ms => {
+  // Мой код
+  return new Promise(resolve => {
     setTimeout(() => {
-      resolved = logger(ms);
+      resolve = logger(ms);
     }, ms);
   });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+export const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
 delay(2000).then(logger); // Resolved after 2000ms
--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,28 +1,35 @@
-const users = [
+export const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
   { name: 'Ajax', active: true },
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
-
-  callback(updatedUsers);
+export const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    resolve(
+      allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      ),
+    );
+  });
 };
 
-const logger = updatedUsers => console.table(updatedUsers);
+export const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
+// Callback
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+// };
+
+// export const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
